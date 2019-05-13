501788e9a1838f607f39783d8a2d816d01df1a66
Yaroslav <yaroslav.afanassiev@gmail.com>
2019-05-13 15:59:17 +0300

    Add: base style component

---
+++ src/components/BaseComponent.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  width: 100%;
+  margin-bottom: .5rem;
+  padding: .6rem;
+  font-size: 1rem;
+  color: #40424b;
+  line-height: 1rem;
+  border: 1px solid #bdc3c7;
+  border-radius: .2rem;
+  box-shadow: none;
+  outline: none;
+`;
+
+export const SubmitButton = styled.button`
+  margin-right: .3rem;
+  padding: .5rem;
+  color: #40424b;
+  border: 1px solid #bdc3c7;
+  border-radius: .2rem;
+  box-shadow: none;
+  cursor: pointer;
+`;
+
+export const CancelButton = styled.button`
+  margin-left: .3rem;
+  padding: 0;
+  font-size: inherit;
+  color: #3498db;
+  cursor: pointer;
+`;
